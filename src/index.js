fb33dfd5f1ecfdce50d545886b01ddb9c1e691aa
Jacob Bare <jacob@limit0.io>
2018-08-14 15:43:01 -0500

    Add graceful shutdown

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const env = require('./env');
 const pkg = require('../package.json');
 const app = require('./app');
-const mongoose = require('./connections/mongoose');
+const { mongoose } = require('./connections');
 
 const { PORT } = env;
 
-mongoose.then(() => {
-  app.listen(PORT, () => {
-    process.stdout.write(`Express app '${pkg.name} v${pkg.version}' listening on port ${PORT}\n`);
-  });
+const server = app.listen(PORT, () => {
+  process.stdout.write(`Express app '${pkg.name} v${pkg.version}' listening on port ${PORT}\n`);
 });
+
+function graceful(options) {
+  const { cleanup, exit } = options;
+  if (cleanup) {
+    const actions = [server.close, mongoose.disconnect];
+    actions.forEach((close, i) => {
+      try {
+        close(() => {
+          if (i === actions.length - 1) process.exit();
+        });
+      } catch (e) {
+        if (i === actions.length - 1) process.exit();
+      }
+    });
+  }
+  if (exit) process.exit();
+}
+
+process.on('exit', graceful.bind(null, { cleanup: true }));
+process.on('SIGINT', graceful.bind(null, { exit: true }));
+process.on('SIGTERM', graceful.bind(null, { exit: true }));
+
+module.exports = server;
