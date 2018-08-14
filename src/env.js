57dd2d2655a4cb1be2932cc130cb8481a98ff3c8
Jacob Bare <jacob@limit0.io>
2018-08-14 14:33:28 -0500

    Add MONGO_DSN to env.js

--- src/env.js
+++ src/env.js
@@ -1,8 +1,17 @@
+const { isURL } = require('validator');
 const {
   cleanEnv,
   port,
+  makeValidator,
 } = require('envalid');
+
+const mongodsn = makeValidator((v) => {
+  const opts = { protocols: ['mongodb'], require_tld: false, require_protocol: true };
+  if (isURL(v, opts)) return v;
+  throw new Error('Expected a Mongo DSN string startng with mongodb://');
+});
 
 module.exports = cleanEnv(process.env, {
   PORT: port({ desc: 'The port that Express will listen on.', default: 5675 }),
+  MONGO_DSN: mongodsn({ desc: 'The MongoDB DSN to connect to.' }),
 });
