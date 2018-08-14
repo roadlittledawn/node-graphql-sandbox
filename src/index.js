fc6b4f08fcb7877dccc496610e134cb463a871fb
Jacob Bare <jacob@limit0.io>
2018-08-14 07:34:53 -0500

    Create initial express app and serve from index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-process.stdout.write('Hello, world!');
+const pkg = require('../package.json');
+const app = require('./app');
+
+const { PORT } = process.env;
+
+const server = app.listen(PORT, () => {
+  process.stdout.write(`Express app '${pkg.name} v${pkg.version}' listening on port ${PORT}\n`);
+});
+
+module.exports = server;
