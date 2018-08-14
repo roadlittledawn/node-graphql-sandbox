1ecf450ec35b0691f92127fb27b3c7a863c8ea2a
Jacob Bare <jacob@limit0.io>
2018-08-14 10:05:33 -0500

    Actually test that the server is listening

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,20 @@
-const index = require('../src/index');
+const app = require('../src/app');
+const { PORT } = require('../src/env');
+const sandbox = sinon.createSandbox();
 
 describe('index', () => {
-  it('should load.', async () => {
+  let index;
+  before(() => {
+    sandbox.spy(app, 'listen');
+    index = require('../src/index');
   });
   after(() => {
     // Close the server.
     index.close();
+  });
+
+  it('should listen using the PORT value.', async () => {
+    sandbox.assert.calledOnce(app.listen);
+    sandbox.assert.calledWith(app.listen, PORT);
   });
 });
