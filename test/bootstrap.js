f74b86254cce1dad10fb1caab7410595b6a049d8
Jacob Bare <jacob@limit0.io>
2018-08-14 16:05:55 -0500

    Wait for services before running tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -3,4 +3,29 @@ global.request = require('supertest');
 global.sinon = require('sinon');
 global.expect = chai.expect;
 
+const { mongoose } = require('../src/connections');
+
 chai.use(require('chai-as-promised'));
+
+const waitForMongoose = () => new Promise((resolve, reject) => {
+  mongoose.on('connected', resolve);
+  mongoose.on('error', reject);
+});
+
+const waitForServices = () => Promise.all([
+  waitForMongoose(),
+]);
+
+waitForServices().then(run).catch((err) => {
+  setImmediate(() => { throw err });
+});
+
+const closeMongoose = () => new Promise((resolve, reject) => {
+  mongoose.close(true, (err) => (err ? reject(err) : resolve()));
+});
+
+const closeServices = () => Promise.all([
+  closeMongoose(),
+]);
+
+after(() => closeServices());
