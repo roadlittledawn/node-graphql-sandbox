76f711e3bfe3ab499d6186f98f5da1268c2b848c
Jacob Bare <jacob@limit0.io>
2018-08-14 14:49:57 -0500

    Add mongoose debug env var

--- src/env.js
+++ src/env.js
@@ -2,6 +2,7 @@ const { isURL } = require('validator');
 const {
   cleanEnv,
   port,
+  bool,
   makeValidator,
 } = require('envalid');
 
@@ -14,4 +15,5 @@ const mongodsn = makeValidator((v) => {
 module.exports = cleanEnv(process.env, {
   PORT: port({ desc: 'The port that Express will listen on.', default: 5675 }),
   MONGO_DSN: mongodsn({ desc: 'The MongoDB DSN to connect to.' }),
+  MONGOOSE_DEBUG: bool({ desc: 'Whether to enable Mongoose query debugging.', default: false }),
 });
