947b5e08eae1a82d1c82dfcbd2cb67a32cd5d176
Jacob Bare <jacob@limit0.io>
2018-08-14 07:50:14 -0500

    Ad gulpfile and set gulp as start command

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const { spawn } = require('child_process');
+
+const watchedPaths = [
+  'src/**/*.js',
+  'src/**/*.graphql',
+];
+
+let node;
+
+gulp.task('serve', ['lint'], function (cb) {
+  if (node) node.kill();
+
+  node = spawn('node', ['src/index.js'], { stdio: 'inherit' });
+  node.on('close', function (code) {
+    if (code === 8) {
+      cb(code);
+      console.log('Error detected, waiting for changes...');
+    }
+  });
+  cb();
+});
+
+gulp.task('watch', function () {
+  gulp.watch(watchedPaths, ['serve']);
+});
+
+gulp.task('lint', function (cb) {
+  const lint = spawn('./node_modules/.bin/eslint', ['src/**/*.js'], { stdio: 'inherit' });
+  lint.on('close', function (code) {
+    if (code === 8) {
+      cb(code);
+      gulp.log('Error detected, waiting for changes...');
+    }
+    cb();
+  });
+});
+
+gulp.task('default', ['watch', 'serve']);
+
+// clean up if an error goes unhandled.
+process.on('exit', function () {
+  if (node) node.kill();
+});
